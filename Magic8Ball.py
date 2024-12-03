35271d4512475f29fd6085edf02787953fc985ed
ellaurrr <ella.g.ratana@gmail.com>
2024-12-03 14:25:29 -0800

    Magic8Ball.py

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,24 +1,20 @@
 import random
 import time
 
-def question():
-    print("what is your question?")
-
-
-def quit():
+def quit():    #function to send a quit message in the program
     print("ok bye!")
 
-
+#list for the possible responses
 responseList = ["it is certain","it is decidedly so","without a doubt","yes definitely","you may rely on it",
             "as i see it, yes", "most likely", "outlook good", "yes", "signs point to yes", "dont count on it",
             "my reply is no", "my sources say no", "outlook not so good", "very doubtful", "reply hazy, try again",
             "ask again later","better not tell you now","cannot predict now","concentrate and ask again"]
 
 print("welcome to the magic 8 ball!")
-while (True):
+while (True): #true while loop that will only leave with a break
     print("would you like to ask a question? y/n")
     play = input()
-    if (play == 'y'):
+    if (play == 'y' or play == "Y"):
         print("what is your question?")
         question = input()
         time.sleep(2)
@@ -27,9 +23,9 @@ while (True):
         response = random.choice(responseList)
         print(response)
 
-    elif (play == 'n'):
+    elif (play == 'n' or play == "N"):
         quit()
-        break
+        break #break to leave loop
 
-    else:
+    else: #catches all non "y/Y" or "n/N" responses
         print("thats not the what im looking for! try again with y or n")
