bd341d06e101430d05cc183115228a4889347aaa
ellaurrr <ella.g.ratana@gmail.com>
2024-12-27 21:19:38 -0800

    TipCalculator.py

--- TipCalculator.py
+++ TipCalculator.py
@@ -8,30 +8,35 @@ def calculateTip(total, tip_percentage):  # calculates the tip based on user inp
     elif tip_percentage == 20:
         total += total * 0.2
     else:
-        print("invalid input, no tip charged.")
+        print("Invalid input, no tip charged.")
     return total
 
+def calculateSum(): #function to calculate sum
+    total = 0
+    for i in range(items):  # loops through all items
+        food_name = input(f"What was the name of item {i + 1}? ")
+        item_cost = float(input(f"What was the cost of the {food_name}? "))
+        foodItem.append(food_name)  # stores item name in the list
+        cost.append(item_cost)  # stores item cost in the list
+    for i in range(items): #loops to add costs into a total sum
+        total += cost[i]
+    return total
 
-print("welcome to the tip calculator!")
-items = int(input("how many items did you purchase? "))  # asks user how many items
+print("Welcome to the tip calculator!")
+items = int(input("How many items did you purchase? "))  # asks user how many items
 
 foodItem = []  # initialize empty lists
 cost = []
 totalCost = 0  # initialize total sum
 
-for i in range(items):  # loops through all items
-    food_name = input(f"what was the name of item {i+1}? ")
-    item_cost = float(input(f"what was the cost of the {food_name}? "))
-    foodItem.append(food_name)  #stores item name in the list
-    cost.append(item_cost)  # stores item cost in the list
-    totalCost += item_cost  #sums up the cost
+totalCost = calculateSum()
 
 # add tax to the total cost
 totalCost += totalCost * 0.1
 
 # ask for tip
-tip_percentage = int(input("how much would you like to tip? 10%, 15%, 18%, or 20%? "))
+tip_percentage = int(input("How much would you like to tip? 10%, 15%, 18%, or 20%? "))
 total_cost = calculateTip(totalCost, tip_percentage)
 
-# display the total cost
-print(f"your total sum, including tax and tip, equals: ${total_cost:.2f}") #printf statement to ensure 2 decimal spaces (learned this from codeQuest projects)
+# display the total cost (learned printf statements from outside source)
+print(f"Your total sum, including tax and tip, equals: ${total_cost:.2f}")
