95714fbb77340a2a83b8ccd28cfb6f0ab9ba777c
ellaurrr <ella.g.ratana@gmail.com>
2025-01-03 11:30:45 -0800

    TipCalculator.py

--- TipCalculator.py
+++ TipCalculator.py
@@ -1,3 +1,4 @@
+
 def calculateTip(total, tip_percentage):  # calculates the tip based on user input
     if tip_percentage == 10:
         total += total * 0.1
@@ -8,26 +9,31 @@ def calculateTip(total, tip_percentage):  # calculates the tip based on user inp
     elif tip_percentage == 20:
         total += total * 0.2
     else:
-        print("Invalid input, no tip charged.")
+        print("invalid input, no tip charged.")
     return total
 
 def calculateSum(): #function to calculate sum
     total = 0
-    if (items != 0):
-        for i in range(items):  # loops through all items
-            food_name = input(f"What was the name of item {i + 1}? ")
-            item_cost = float(input(f"What was the cost of the {food_name}? "))
-            foodItem.append(food_name)  # stores item name in the list
-            cost.append(item_cost)  # stores item cost in the list
-        for i in range(items): #loops to add costs into a total sum
-            total += cost[i]
-        return total
-    else:
-        print("you didnt buy any items silly!")
-        return 0
+    while (True):
+        items = int(input("how many items did you purchase? "))  # asks user how many items
+        if (items > 0):
+            for i in range(items):  # loops through all items
+                food_name = input(f"What was the name of item {i + 1}? ")
+                item_cost = float(input(f"What was the cost of the {food_name}? "))
+                foodItem.append(food_name)  # stores item name in the list
+                cost.append(item_cost)  # stores item cost in the list
+            for i in range(items): #loops to add costs into a total sum
+                total += cost[i]
+            return total
+            break
+        elif (items<0): #checks to see if items inputted are negative
+            print("you cant have negative items, try again!")
+        elif (items == 0): #checks to see if items inputted is zero
+            print("you cant have 0 items, try again!")
+        else: #checks to see if items inputted is not a valid number
+            print("invalid input, pls give me the # of items you purchased")
 
-print("Welcome to the tip calculator!")
-items = int(input("How many items did you purchase? "))  # asks user how many items
+print("welcome to the tip calculator!")
 
 foodItem = []  # initialize empty lists
 cost = []
@@ -39,8 +45,8 @@ totalCost = calculateSum()
 totalCost += totalCost * 0.1
 
 # ask for tip
-tip_percentage = int(input("How much would you like to tip? 10%, 15%, 18%, or 20%? "))
+tip_percentage = int(input("how much would you like to tip? 10%, 15%, 18%, or 20%? "))
 total_cost = calculateTip(totalCost, tip_percentage)
 
 # display the total cost (learned printf statements from outside source)
-print(f"Your total sum, including tax and tip, equals: ${total_cost:.2f}")
+print(f"your total sum, including tax and tip, equals: ${total_cost:.2f}")
